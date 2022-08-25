167637d70d3660c345f0bb0e0e5cb1518966eb92
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-25 13:25:03 +0530

    it is udemy exercise.

--- src/FlourPacker.java
+++ src/FlourPacker.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class FlourPacker {
+public class FlourPacker {
+    public static boolean canPack(int bigCount, int smallCount, int goal){
+        if ((bigCount < 0) || (smallCount < 0) || (goal < 0)) {
+            return false;
+        }else {
+
+        while (bigCount > 0 && goal >= 5) {
+            goal -= 5;
+            bigCount--;
+        }
+        return smallCount >= goal;
+    }}
 }
